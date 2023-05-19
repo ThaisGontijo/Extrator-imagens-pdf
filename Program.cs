9979eab67d16c26420a7b04289ccb58b13d6a96e
Thais Fernanda Parreira Gontijo <thais.gontijo@placcar.com.br>
2023-05-19 19:51:50 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+using GroupDocs.Parser;
+using GroupDocs.Parser.Data;
+using GroupDocs.Parser.Options;
+
+// Extract images from PDF using C#
+using (Parser parser = new Parser("C:\\Thais\\Carta - Pernoita Velada Atividade Sbado de Manh.pdf"))
+{
+    IEnumerable<PageImageArea> images = parser.GetImages();
+    // Check if image extraction is supported
+    if (images == null)
+    {
+        Console.WriteLine("Images extraction isn't supported");
+        return;
+    }
+
+    ImageOptions options = new ImageOptions(ImageFormat.Jpeg);
+    int imageNumber = 0;
+
+    // Iterate over retrieved images
+    foreach (PageImageArea image in images)
+    {
+        // Save Images
+        image.Save("C:\\Thais\\image-" + imageNumber.ToString() + ".jpeg", options);
+        imageNumber++;
+    }
+}
